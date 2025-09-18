38a5e4edcc1f5a451ea3d8fc731b2c08e199fbf0
shayantaqi <80316026+shayantaqi@users.noreply.github.com>
2025-09-18 13:10:55 +0330

    First commit

---
+++ input/raw_data/rename_data.bat
@@ -0,0 +1,14 @@
+@echo off
+cd ./input/raw_data/
+rename "EERA Public SchoolCBE - Data Entry Tool AF25 - R1.xlsx" "Tool 0.EERA Public SchoolCBE - Data Entry Tool R5.xlsx"
+rename "EERA Public School - HeadmasterPrinciple Interview KDR AF25 - R1.xlsx" "Tool 11.EERA Public School - HeadmasterPrinciple Interview KDR R5.xlsx"
+rename "EERA Public School - HeadmasterPrinciple Interview AF25 - R1.xlsx" "Tool 1.EERA Public School - HeadmasterPrinciple Interview R5.xlsx"
+rename "EERA Public School - Light Tool AF25 - R1.xlsx" "Tool 2.EERA Public School - Light Tool R5.xlsx"
+rename "EERA Public School - Student Document & Headcount AF25 - R1.xlsx" "Tool 3.EERA Public School - Student Document & Headcount R5.xlsx"
+rename "EERA Public School - Teacher Tool AF25 - R1.xlsx" "Tool 4.EERA Public School - Teacher Tool R5.xlsx"
+rename "EERA Public School - WASH Observation AF25 - R1.xlsx" "Tool 5.EERA Public School - WASH Observation R5.xlsx"
+rename "EERA Public SchoolCBE - Parent Tool AF25 - R1.xlsx" "Tool 6.EERA Public SchoolCBE - Parent Tool R5.xlsx"
+rename "EERA Public SchoolCBE - Shura Tool AF25 - R1.xlsx" "Tool 7.EERA Public SchoolCBE - Shura Tool R5.xlsx"
+rename "EERA CBE - Class Level Tool AF25 - R1.xlsx" "Tool 8.EERA CBE - Class Level Tool R5.xlsx"
+rename "EERA CBE - IP Level Tool AF25 - R1.xlsx" "Tool 9.EERA CBE - IP Level Tool R5.xlsx"
+pause
---
+++ input/tools/rename_tool.bat
@@ -0,0 +1,13 @@
+@echo off
+cd ./input/tools/
+rename "EERA Public School_CBE - Data Entry Tool - R5.xlsx" "Tool 0.EERA Public School_CBE - Data Entry Tool - R5.xlsx"
+rename "EERA Public School - Headmaster_Principle Interview KDR AF25 - R1.xlsx" "Tool 11.EERA Public School - Headmaster Principle Interview KDR- R5.xlsx"
+rename "EERA Public School - Headmaster_Principle Interview AF25 - R1.xlsx" "Tool 1.EERA Public School - Headmaster Principle Interview - R5.xlsx"
+rename "EERA Public School - Light Tool - R5.xlsx" "Tool 2.EERA Public School - Light Tool - R5.xlsx"
+rename "EERA Public School - Student Docs & Headcount - R5.xlsx" "Tool 3.EERA Public School - Student Document & Headcount - R5.xlsx"
+rename "EERA Public School - Teacher Tool - R5.xlsx" "Tool 4.EERA Public School - Teacher Tool - R5.xlsx"
+rename "EERA Public School - WASH Observation - R5.xlsx" "Tool 5.EERA Public School - WASH Observation - R5.xlsx"
+rename "EERA Public School_CBE - Parent Tool - R5.xlsx" "Tool 6.EERA Public School_CBE - Parent Tool - R5.xlsx"
+rename "EERA Public School_CBE - Shura Tool - R5.xlsx" "Tool 7.EERA Public School_CBE - Shura Tool - R5.xlsx"
+rename "EERA CBE_Class Level Tool_R5.xlsx" "Tool 8.EERA CBE - Class Level Tool - R5.xlsx"
+rename "EERA CBE_IP Level Tool_R5.xlsx" "Tool 9.EERA CBE - IP Level Tool - R5.xlsx"
